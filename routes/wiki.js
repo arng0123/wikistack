15b690bdafbaba09903ca10d4353f811365762ce
Olivia Kasmin <oliviakasmin@gmail.com>
2020-06-25 16:25:11 -0400

    html changes

--- routes/wiki.js
+++ routes/wiki.js
@@ -1,4 +1,21 @@
 const express = require('express')
 const wikiRouter = express.Router()
+const addPage = require('../views/addPage')
+
+// retrieve all wiki pages
+wikiRouter.get('/', (req,res,next) => {
+  res.redirect('/')
+})
+
+// submit a new page to the database
+wikiRouter.post('/', (req, res, next) => {
+
+})
+
+// retrieve the "add a page" form
+wikiRouter.get('/add/', (req, res, next) => {
+  res.send(addPage())
+})
+
 
 module.exports = wikiRouter
