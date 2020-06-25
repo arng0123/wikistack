d2df86becf07288e39573ce58841fa0b6d806f5e
Olivia Kasmin <oliviakasmin@gmail.com>
2020-06-25 12:40:00 -0400

    almost done setup

--- app.js
+++ app.js
@@ -5,3 +5,12 @@ app.use(morgan ('dev'))
 // maybe need to change __dirname
 app.use(express.static(__dirname + "/public"));
 app.use(express.urlencoded({ extended: false }));
+
+app.get('/', (req,res) => {
+  console.log('hello world')
+})
+
+let port = 1337
+app.listen(port, ()=>{
+  console.log(`app listening in port ${port}`)
+})
